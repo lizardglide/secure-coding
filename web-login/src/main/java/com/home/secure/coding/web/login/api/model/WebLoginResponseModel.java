a9d3ee412e1a4b13db1a5b729d46e31a531d44c0
lizardglide <starphantom@protonmail.com>
2023-03-17 00:21:55 +0100

    weblogincontroller added, added security config

---
+++ web-login/src/main/java/com/home/secure/coding/web/login/api/model/WebLoginResponseModel.java
@@ -0,0 +1,14 @@
+package com.home.secure.coding.web.login.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class WebLoginResponseModel {
+    private String message;
+}
